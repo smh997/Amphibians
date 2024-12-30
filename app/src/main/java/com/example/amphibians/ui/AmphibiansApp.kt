185c8753a36fc672f6ed210f538f69c1783d0b5d
smh10 <smh10.mh@gmail.com>
2024-12-30 16:17:13 -0500

    AmphibiansApp basic implementation is done.

---
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
@@ -0,0 +1,51 @@
+package com.example.amphibians.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import com.example.amphibians.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibiansApp(modifier: Modifier = Modifier) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { AmphibiansTopAppBar(scrollBehavior) }
+    ) { innerPadding ->
+        Surface (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ){
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibiansTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    TopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        modifier = modifier
+    )
+}

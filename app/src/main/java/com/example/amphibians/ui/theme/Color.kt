58444e61b1bfb08901771cbc9812b74606602481
smh10 <smh10.mh@gmail.com>
2025-01-02 16:15:06 -0500

    Colors and Theme are updated.

--- app/src/main/java/com/example/amphibians/ui/theme/Color.kt
+++ app/src/main/java/com/example/amphibians/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.amphibians.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_background = Color(0xFFFCFDF6)
+val md_theme_light_surface = Color(0xFFFCFDF6)
+val md_theme_light_surfaceVariant = Color(0xFFDEE5D8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val md_theme_dark_background = Color(0xFF1A1C19)
+val md_theme_dark_surface = Color(0xFF1A1C19)
+val md_theme_dark_surfaceVariant = Color(0xFF424940)
